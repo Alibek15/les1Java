2c9096798f2b051411e337e5771ab7fbf093e5c7
Alibek <52044412+Alibek15@users.noreply.github.com>
2022-08-26 14:44:45 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //System.out.println("Hello world");
+        //Целочисленные
+        //byte -127 128
+        //short -32768 32767
+        //int 2 ...
+        //long 10^32
+
+        //float 5f
+        //double 12
+
+        //boolean - true false
+        //char - 'A'
+
+    /*    byte a=10;
+        System.out.println(a);
+
+        short b=3400;
+        System.out.println(b);
+
+        int c=20004;
+
+        System.out.println(c);
+
+        long d=3000L;
+
+        System.out.println(d);
+
+        float f = 1243.453f;
+
+        System.out.println(f);
+
+        double e = 123454.33255;
+
+        System.out.println(e);*/
+
+        /*char c = 'A';
+
+        boolean f = true;
+
+        System.out.println(c);
+
+        System.out.println(f);*/
+        /* stack(byte short int long) heap perm generation(static context) -- Java Memory model*/
+        String name="Alibek";
+        /* name from stack link to the heap Alibek value*/
+        
+    }
+}
